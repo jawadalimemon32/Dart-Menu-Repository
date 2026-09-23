e4d7bff141aefdc97948dfb27825f7e44eced342
jawadalimemon32 <memonjawad32@gmail.com>
2026-09-23 22:05:52 +0500

    Add StudentPercentage.dart to calculate percentage

---
+++ StudentPercentage.dart
@@ -0,0 +1,11 @@
+void main() {
+ int english = 70;
+ int math = 80;
+ int computer = 90;
+ 
+ int totalmarks = english+math+computer;
+ double percentage = (totalmarks / 300) * 100;
+ 
+ print("totalmarks: $totalmarks");
+  print("Percentage: $percentage%");
+}
