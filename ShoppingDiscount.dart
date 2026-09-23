9ae0aadcd4c8674524880f053906703e841d1966
jawadalimemon32 <memonjawad32@gmail.com>
2026-09-23 23:45:39 +0500

    Create ShoppingDiscount.dart

---
+++ ShoppingDiscount.dart
@@ -0,0 +1,9 @@
+void main() {
+int price = 2000;
+
+double discount = price *0.10;
+double finalprice = price - discount;
+
+print('Discount: Rs. $discount');
+  print('Final Price: Rs. $finalprice');
+}
